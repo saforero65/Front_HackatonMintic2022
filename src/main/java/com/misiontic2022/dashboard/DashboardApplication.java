8445391003e3c2456e63a5dd87b353d2e8a1cc59
marsantiagope <marsantiagope@gmail.com>
2021-11-18 14:57:33 -0500

    Primer commit

---
+++ src/main/java/com/misiontic2022/dashboard/DashboardApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic2022.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+
+}
